e9a536165373ef9a5ed76878199e1886ae5035e4
Deividas <deividas.aldonis.dev@gmail.com>
2022-12-17 15:01:32 +0200

    Add ability to select an image

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-
 const container = document.querySelector(".container");
+const controls = document.querySelector(".controls");
+
+let selected = null;
+
+const setSelectedImage = (e) => {
+  const slicedImage = e.target.closest(".sliced-image");
+  if (!slicedImage) return;
+
+  const id = Number(slicedImage.dataset.id);
+
+  console.log(id);
+  document.querySelectorAll(".sliced-image").forEach((img, i) => {
+    console.log(i);
+    if (i === id) {
+      img.classList.add("selected");
+    } else {
+      img.classList.remove("selected");
+    }
+  });
+};
 
 button.addEventListener("click", () => {
   //   const inputValue = input.value.trim();
@@ -18,6 +37,7 @@ button.addEventListener("click", () => {
   image.addEventListener("load", () => {
     const div = document.createElement("div");
     div.classList.add("image");
+    div.addEventListener("click", setSelectedImage);
 
     div.style.height = image.height + "px";
     div.style.width = image.width + "px";
@@ -25,10 +45,11 @@ button.addEventListener("click", () => {
 
     for (let i = 0; i < 4; i++) {
       const slicedImage = document.createElement("div");
-      div.appendChild(slicedImage);
-
+      slicedImage.classList.add("sliced-image");
+      slicedImage.dataset.id = i;
       slicedImage.style.backgroundImage = `url(${image.src})`;
       slicedImage.style.backgroundSize = `${image.width}px ${image.height}px`;
+
       if (i === 0) {
         slicedImage.style.backgroundPositionX = `${image.width}px`;
         slicedImage.style.backgroundPositionY = `${image.height}px`;
@@ -45,8 +66,16 @@ button.addEventListener("click", () => {
         slicedImage.style.backgroundPositionX = `-${image.width / 2}px`;
         slicedImage.style.backgroundPositionY = `${image.height / 2}px`;
       }
+
+      div.appendChild(slicedImage);
     }
 
-    container.appendChild(div);
+    container.insertAdjacentElement("afterbegin", div);
+    controls.classList.remove("hide");
   });
+});
+
+controls.addEventListener("click", (e) => {
+  const arrow = e.target.closest("svg");
+  if (!arrow) return;
 });
